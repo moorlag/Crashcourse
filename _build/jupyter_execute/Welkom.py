1ff57bbe6d1feff0a9c205669e2ac289464d4dba
moorlag <r.moorlag@gmail.com>
2022-05-31 18:18:00 +0000

    First commit
    de

---
+++ _build/jupyter_execute/Welkom.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Welkom bij de CrashCourse Python
+
+# Dit is de introductie Python. 
+# Hieronder vind je de eerste commando's voor je eerste Python Script
+
+# In[1]:
+
+
+2 + 2
+
+
+# In[ ]:
+
+
+
+
